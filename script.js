a6064d5572e89150ead205666e8db3f3a55ec1e6
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-14 22:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(event) {
+  var text = document.createTextNode("DOM load success");
+  document.body.appendChild(text);
+});
